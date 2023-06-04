6fbeca13a448676139213ba2f59458011c32615c
@AdityaPutra17 <noleptim@gmail.com>
2023-06-04 21:19:13 +0700

    Login page

--- SaungEling/App.js
+++ SaungEling/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Viewstart from './splashscreen/Viewstart';
+import Viewlogin from './LoginScreen/Viewlogin';
 
 export default function App() {
   return (
-    <Viewstart/>
+    // <Viewstart/>
+    <Viewlogin/>
   );
 }
 
