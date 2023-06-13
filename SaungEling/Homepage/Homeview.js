3eb5052dfd2daab98aa3b56902b668cbc0857767
@AdityaPutra17 <noleptim@gmail.com>
2023-06-13 09:12:31 +0700

    terbaru 13/06/23

--- SaungEling/Homepage/Homeview.js
+++ SaungEling/Homepage/Homeview.js
@@ -1,50 +1,54 @@
-import React from 'react';
-import {View, StyleSheet, SafeAreaView, StatusBar} from 'react-native';
+import React, { Component } from 'react';
+import {View, Text,StyleSheet, SafeAreaView, StatusBar, Image, TouchableOpacity} from 'react-native';
+import {FontAwesome5} from '@expo/vector-icons';
 import Searchbar from './View/searchbar';
-import Logo from './View/logo';
-import Navbar from './View/navbar';
+import Cardmenu from './View/Cardmenu';
 import Content from './View/content';
+const ijo='#04450B';
 
-const Homeview = () => {
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.logo}>
-                <Logo/>
+class Homeview extends Component {
+    render() {
+        return (
+        <SafeAreaView style={{flex:1, paddingHorizontal:20, marginTop:10}}>
+            <View style={{flex:1}}>
+                <View style={styles.atas}>
+                    <Image source={require('../assets/logo.png')}/>
+                    <TouchableOpacity style={styles.bgprofile}>
+                        <FontAwesome5 name="user-alt" size={24} color={ijo} />
+                    </TouchableOpacity>
+                </View>
+                <Text style={{fontSize:32, fontWeight:'bold', color:ijo, marginTop:10}}>Hi, Elingers</Text>
+                <Text style={{color:ijo, fontSize:16}}>Jelajahi Rasa dan Pesan Sekarang</Text>
             </View>
-            <View style={styles.searchbar}>
+            <View style={{flex:0.3}}>
                 <Searchbar/>
             </View>
-            <View style={styles.navbar}>
-                <Navbar/>
+            <View style={{flex:1}}>
+                <Cardmenu/>
             </View>
-            <View style={styles.content}>
+            <View style={{flex:2}}>
                 <Content/>
             </View>
             <StatusBar/>
         </SafeAreaView>
-    );
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        paddingHorizontal:20,
-        marginTop:10
+    atas:{
+        flexDirection:'row',
+        justifyContent:'space-between'
     },
-    logo:{
-        flex:1,
-        
+    bgprofile:{
+        width:44,
+        height:44,
+        backgroundColor:'#eeeeee',
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:30,
+        marginTop:8, marginRight:8
     },
-    searchbar:{
-        flex:0.3,
-    },
-    navbar:{
-        flex:1,
-    },
-    content:{
-        flex:2,
-    }
-
 })
 
 export default Homeview;
